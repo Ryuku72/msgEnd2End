e17f9a42b8621f5620db0c045ddf2fadfb07ddbe
Ryuku72 <jaybshinsen@hotmail.com>
2024-07-03 23:44:09 +0800

    bash update attempt 3

--- git_commit.sh
+++ git_commit.sh
@@ -26,13 +26,7 @@ REVIEW_ENTRY="- [$LATEST_COMMIT_HASH]($REPO_URL/commit/$LATEST_COMMIT_HASH) $COM
 if [ ! -f CHANGELOG.md ]; then
   echo -e "## Changelog\n\n$CHANGELOG_ENTRY" > CHANGELOG.md
 else
-  # Check if ## Changelog exists and append if so, otherwise add it
-  if grep -q "## Changelog" CHANGELOG.md; then
-    sed -i "/## Changelog/a\\
-$CHANGELOG_ENTRY\n" CHANGELOG.md
-  else
-    echo -e "## Changelog\n\n$CHANGELOG_ENTRY\n$(cat CHANGELOG.md)" > CHANGELOG.md
-  fi
+  sed -i "1s/^/$CHANGELOG_ENTRY\n\n/" CHANGELOG.md
 fi
 
 # Update review.md with the provided review title
@@ -44,13 +38,7 @@ else
   if [ ! -f "$REVIEW_FILE" ]; then
     echo -e "$REVIEW_TITLE\n\n$REVIEW_ENTRY" > "$REVIEW_FILE"
   else
-    # Check if the review title exists and append if so, otherwise add it
-    if grep -q "$REVIEW_TITLE" "$REVIEW_FILE"; then
-      sed -i "/$REVIEW_TITLE/a\\
-$REVIEW_ENTRY\n" "$REVIEW_FILE"
-    else
-      echo -e "$REVIEW_TITLE\n\n$REVIEW_ENTRY\n$(cat "$REVIEW_FILE")" > "$REVIEW_FILE"
-    fi
+    echo -e "$REVIEW_TITLE\n\n$REVIEW_ENTRY\n$(cat "$REVIEW_FILE")" > "$REVIEW_FILE"
   fi
 fi
 
