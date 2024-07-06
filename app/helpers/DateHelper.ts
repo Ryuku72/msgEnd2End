1dc9345b79aec67598032920a395ea38b60f7142
Ryuku72 <jaybshinsen@hotmail.com>
2024-07-06 19:31:01 +0800

    fix date

--- app/helpers/DateHelper.ts
+++ app/helpers/DateHelper.ts
@@ -1,7 +1,7 @@
 export function CreateDate(date: string, time?: boolean) {
   if (!date) return '';
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const newDate = new Date(date.charAt(date.length - 1) === 'Z' ? date : date + 'Z');
+  const newDate = new Date(date + 'Z');
   const year = new Intl.DateTimeFormat('en-au', { day: '2-digit', month: '2-digit', year:'numeric', timeZone }).format(newDate);
   const hours = new Intl.DateTimeFormat('en-au', { minute: '2-digit', hour: '2-digit', timeZone }).format(newDate);
   if (time) return `${year} at ${hours} `;
@@ -11,7 +11,7 @@ export function CreateDate(date: string, time?: boolean) {
 
 export function CreateTimeOnly(date: string) {
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const newDate = new Date(date.charAt(date.length - 1) === 'Z' ? date : date + 'Z');
+  const newDate = new Date(date + 'Z');
   const hours = new Intl.DateTimeFormat('en-au', { minute: '2-digit', hour: '2-digit', timeZone }).format(newDate);
   return `${hours}`;
 }
